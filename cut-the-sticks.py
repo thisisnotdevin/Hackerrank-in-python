461fd95d389438a51dacb1dd7cd13789095d6e38
Devin Lin <66978846+thisisnotdevin@users.noreply.github.com>
2023-02-08 13:49:30 -0500

    Create cut-the-sticks.py

---
+++ cut-the-sticks.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def cutTheSticks(arr):
+    arr = sorted(arr)
+    result = []
+    while arr:
+        result.append(len(arr))
+        arr = [i - arr[0] for i in arr if i - arr[0] > 0]
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
